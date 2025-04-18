867f7c3e8c705236723e3b65e4b835e7355a97ef
Karen Huang <thinknfix2021@gmail.com>
2025-04-18 22:03:31 +0800

    add: main css file 引入 tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css' //目前的主要 css file
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
