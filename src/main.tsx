669cc53e87206b120c40d9237c0136848b56b10a
Karen Huang <thinknfix2021@gmail.com>
2025-04-19 11:51:59 +0800

    fix: main 專案進入點，整合 App 路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css"; //目前的主要 css file
-import LoginPage from "./LoginPage";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <LoginPage />
+      <App />
     </BrowserRouter>
   </StrictMode>
 );
