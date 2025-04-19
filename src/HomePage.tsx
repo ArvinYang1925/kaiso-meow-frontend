8644b0a3e51e27f94c0f6f90ed368302e9d4063a
Karen Huang <thinknfix2021@gmail.com>
2025-04-19 16:15:01 +0800

    add: 跳轉首頁帶入使用者資訊-從localstorage

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
+
+type loginResponseData = {
+  name: string;
+  id: string;
+  email: string;
+  phoneNumber: string;
+  role: string;
+};
+
 export const HomePage: React.FC = () => {
+  const [userName, setUserName] = useState("");
+
+  // 從 localStorage 取出資料並解析
+  const userInfoString = localStorage.getItem("userInfo");
+  const userInfo: loginResponseData = userInfoString
+    ? JSON.parse(userInfoString)
+    : null;
+
+  useEffect(() => {
+    if (userInfo) {
+      setUserName(userInfo.name);
+    }
+  }, [userInfo]);
+
   return (
     <>
+      {userName !== "" && <h1>嗨！{userName}，你好啊！</h1>}
       <h1>歡迎來到首頁</h1>
     </>
   );
