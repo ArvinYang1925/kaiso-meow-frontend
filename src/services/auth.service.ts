1b42b531f9a892a3bed79fad312b48b01a674dd8
Karen Huang <thinknfix2021@gmail.com>
2025-04-20 14:11:33 +0800

    test: 💍 test
    test for git cz
    
    BREAKING CHANGE: 🧨 nothing
    
    ✅ Closes: git push

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,4 +1,4 @@
-import axios from '../services/axiosInstance'; //很重要，這邊必須確認呼叫到的是 axiosInstance
+import axios from '../services/axiosInstance'; //很重要，這邊必須確認呼叫到的是 axiosInstance!!
 
 /** 使用者註冊 */
 type RegisterUserDataModel = {
