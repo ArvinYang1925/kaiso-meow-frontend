7addb97fc7367c9439645499ec61ff4ad5d93fff
Karen Huang <thinknfix2021@gmail.com>
2025-04-27 13:13:13 +0800

    add: API BaseUrl 改打後端環境

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://express-ts-todolist.onrender.com/', // 替換成你的 API URL
-        changeOrigin: true,  // 用來改變原始的請求頭部
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  /** 處理 cors 的反向代理設定 */
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://express-ts-todolist.onrender.com/', // 替換成你的 API URL
+  //       changeOrigin: true,  // 用來改變原始的請求頭部
+  //       rewrite: (path) => path.replace(/^\/api/, ''),
+  //     },
+  //   },
+  // },
 })
 
