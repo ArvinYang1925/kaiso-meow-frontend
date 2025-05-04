fb5c667b471b6fe89d7abba49d5202c70734421e
Karen Huang <thinknfix2021@gmail.com>
2025-05-04 23:14:27 +0800

    add: 依照後端的白名單加入本地端的CORS設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -12,5 +12,20 @@ export default defineConfig({
       '@': '/src',  // 確保這裡指向了 src 目錄
     },
   },
+  server: {
+    port: 5173, //前端啟動的 port (需要有被後端加入白名單)
+    strictPort: true, // 如果端口被占用，則不自動切換到其他端口
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', //後端啟動的 port
+        changeOrigin: true,
+        secure: false,
+        ws: true
+      }
+    }
+  },
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.API_URL)
+  }
 })
 
