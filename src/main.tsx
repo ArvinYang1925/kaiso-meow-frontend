efc46df9e6ce6a03768e1796a8845b70235ede1d
Karen Huang <thinknfix2021@gmail.com>
2025-05-19 23:47:26 +0800

    fix: 🐛 前端加入正式與開發環境走不同路由

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css"; //目前的主要 css file
 import App from "./app/App";
-import { HashRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 import "./assets/scss/all.scss";
+
+//開發環境使用 BrowserRouter，正式環境用 HashRouter
+const isProd = import.meta.env.MODE === "production";
+const Router = isProd ? HashRouter : BrowserRouter;
+
+console.log('log for router, isProd:',isProd)
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
+    <Router>
       <App />
-    </HashRouter>
+    </Router>
   </StrictMode>
 );
