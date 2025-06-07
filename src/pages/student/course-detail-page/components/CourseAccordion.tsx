517a9a583d2c50b927818ce2f0e5cd43522f1df5
Karen Huang <thinknfix2021@gmail.com>
2025-06-07 16:26:16 +0800

    fix: 🐛 課程詳細頁-章節名稱顯示調整

--- src/pages/student/course-detail-page/components/CourseAccordion.tsx
+++ src/pages/student/course-detail-page/components/CourseAccordion.tsx
@@ -16,11 +16,13 @@ export const CourseAccordion = ({ sections }: CourseAccordionProps) => {
       {[...sections]
         .sort((a, b) => a.orderIndex - b.orderIndex)
         .map((section, index) => (
-          <AccordionItem className="py-6" key={section.id} value={`item-${index + 1}`}>
+          <AccordionItem
+            className="py-6"
+            key={section.id}
+            value={`item-${index + 1}`}
+          >
             {/* <AccordionTrigger> */}
-              <p>
-                第{section.orderIndex}章：{section.title}
-              </p>
+            <p>{section.title}</p>
             {/* </AccordionTrigger> */}
           </AccordionItem>
         ))}
