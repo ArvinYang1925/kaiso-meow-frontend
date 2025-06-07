6281006fd88d86cfa5747509876032233009b369
Karen Huang <thinknfix2021@gmail.com>
2025-06-07 12:52:03 +0800

    feat: 🎸 課程詳細頁接真實API資料及issue#51

--- src/pages/student/course-detail-page/components/CourseAccordion.tsx
+++ src/pages/student/course-detail-page/components/CourseAccordion.tsx
@@ -1,8 +1,8 @@
 import {
   Accordion,
   AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
+  // AccordionTrigger,
+  // AccordionContent,
 } from "@/components/ui/accordion";
 import { Section } from "../course-detail.service";
 
@@ -13,14 +13,15 @@ interface CourseAccordionProps {
 export const CourseAccordion = ({ sections }: CourseAccordionProps) => {
   return (
     <Accordion type="single" collapsible className="w-full">
-      {sections
+      {[...sections]
         .sort((a, b) => a.orderIndex - b.orderIndex)
         .map((section, index) => (
-          <AccordionItem key={section.id} value={`item-${index + 1}`}>
-            <AccordionTrigger><p>第{section.orderIndex}章：{section.title}</p> </AccordionTrigger>
-            <AccordionContent>
-              <p>{section.content}</p>
-            </AccordionContent>
+          <AccordionItem className="py-6" key={section.id} value={`item-${index + 1}`}>
+            {/* <AccordionTrigger> */}
+              <p>
+                第{section.orderIndex}章：{section.title}
+              </p>
+            {/* </AccordionTrigger> */}
           </AccordionItem>
         ))}
     </Accordion>
--- src/pages/student/order-page/store/orderStore.ts
+++ src/pages/student/order-page/store/orderStore.ts
@@ -1,6 +1,10 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
-import { CouponInfo, UserInfo, CourseInfo, OrderStatus, CreateOrderRequestModel, ApplyCouponRequestModel } from "../service/type";
+import {
+    CouponInfo, UserInfo, CourseInfo, OrderStatus,
+    CreateOrderRequestModel, ApplyCouponRequestModel
+} from "../service/type";
 import { applyCoupon, checkoutEcpay, createOrder, createOrderPreview, fetchOrder } from "../service/order.service";
 import { initCouponData, initCourseData, initOrderData, initUserData } from "../constants/initialData";
 
@@ -11,7 +15,7 @@ type OrderInfo = {
     orderPrice: number | null;
     status: OrderStatus;
     paidAt: string;
-}
+};
 
 interface OrderPageState {
     orderData: OrderInfo;
@@ -23,131 +27,146 @@ interface OrderPageState {
 }
 
 interface OrderPageAction {
-    // setIsLoading: (loading: boolean) => void;
     setIsShowDialog: (isShowDialog: boolean) => void;
     resetStore: () => void;
     createOrderPreview: (requestData: CreateOrderRequestModel) => void;
-    applyCoupon: (requestData: ApplyCouponRequestModel) => void;
+    applyCoupon: (requestData: ApplyCouponRequestModel) => Promise<void>
     fetchOrder: (orderId: string) => void;
     createOrder: (requestData: CreateOrderRequestModel) => Promise<string>;
 }
 
 export const useOrderStore = create<OrderPageState & OrderPageAction>()(
-    immer((set, get) => ({
-        userData: initUserData,
-        orderData: initOrderData,
-        couponData: initCouponData,
-        courseData: initCourseData,
-        isLoading: false,
-        isShowDialog: false,
-        setIsShowDialog: (isShowDialog) => {
-            set((state) => {
-                state.isShowDialog = isShowDialog;
-            });
-        },
-        createOrderPreview: async (requestData) => {
-            get().resetStore();
-            set((state) => {
-                state.isLoading = true;
-            });
-            try {
-                const response = await createOrderPreview(requestData);
-                const { originalPrice, orderPrice, status, course, user } = response;
+    persist(
+        immer((set, get) => ({
+            userData: initUserData,
+            orderData: initOrderData,
+            couponData: initCouponData,
+            courseData: initCourseData,
+            isLoading: false,
+            isShowDialog: false,
+            setIsShowDialog: (isShowDialog) => {
+                set((state) => {
+                    state.isShowDialog = isShowDialog;
+                });
+            },
+            createOrderPreview: async (requestData) => {
+                get().resetStore();
+                set((state) => {
+                    state.isLoading = true;
+                });
+                try {
+                    const response = await createOrderPreview(requestData);
+                    const { originalPrice, orderPrice, status, course, user } = response;
 
+                    set((state) => {
+                        state.orderData.orderPrice = orderPrice;
+                        state.orderData.originalPrice = originalPrice;
+                        state.orderData.status = status;
+                        state.courseData = course;
+                        state.userData = user;
+                    });
+                } catch (error) {
+                    console.error('Failed to createOrderPreview', error);
+                    throw error;
+                } finally {
+                    set((state) => {
+                        state.isLoading = false;
+                    });
+                }
+            },
+            applyCoupon: async (requestData) => {
                 set((state) => {
-                    state.orderData.orderPrice = orderPrice;
-                    state.orderData.originalPrice = originalPrice;
-                    state.orderData.status = status;
-                    state.courseData = course;
-                    state.userData = user;
+                    state.isLoading = true;
                 });
-            } catch (error) {
-                console.error('Failed to createOrderPreview', error);
-                throw error;
-            } finally {
+                try {
+                    const response = await applyCoupon(requestData);
+                    const { coupon, order } = response;
+                    set((state) => {
+                        state.couponData = coupon;
+                        state.orderData.orderPrice = order.orderPrice;
+                        state.orderData.originalPrice = order.originalPrice;
+                    });
+                } catch (error: any) {
+                    console.error('Failed to applyCoupon', error.response.data);
+                    throw error;
+                } finally {
+                    set((state) => {
+                        state.isLoading = false;
+                    });
+                }
+            },
+            fetchOrder: async (orderId) => {
                 set((state) => {
-                    state.isLoading = false;
+                    state.isLoading = true;
                 });
-            }
+                try {
+                    const response = await fetchOrder(orderId);
+                    const { coupon, user, course, id, originalPrice, orderPrice, status } = response;
 
-        },
-        applyCoupon: async (requestData) => {
-            set((state) => {
-                state.isLoading = true;
-            });
-            try {
-                const response = await applyCoupon(requestData);
-                const { coupon, order } = response;
+                    set((state) => {
+                        state.couponData = coupon;
+                        state.userData = user;
+                        state.courseData = course;
+                        state.orderData.id = id;
+                        state.orderData.originalPrice = originalPrice;
+                        state.orderData.orderPrice = orderPrice;
+                        state.orderData.status = status;
+                    });
+                } catch (error) {
+                    console.error('Failed to fetchOrder', error);
+                    throw error;
+                } finally {
+                    set((state) => {
+                        state.isLoading = false;
+                    });
+                }
+            },
+            createOrder: async (requestData) => {
                 set((state) => {
-                    state.couponData = coupon;
-                    state.orderData.orderPrice = order.orderPrice;
-                    state.orderData.originalPrice = order.originalPrice;
+                    state.isLoading = true;
                 });
-            } catch (error) {
-                console.error('Failed to applyCoupon', error);
-                throw error;
-            } finally {
-                set((state) => {
-                    state.isLoading = false;
-                });
-            }
-        },
-        fetchOrder: async (orderId) => {
-            set((state) => {
-                state.isLoading = true;
-            });
-            try {
-                const response = await fetchOrder(orderId);
-                const { coupon, user, course, id, originalPrice, orderPrice, status } = response;
+                try {
+                    const newOrderResData = await createOrder(requestData);
+                    const { id: orderId, orderPrice } = newOrderResData.data;
+                    if (orderPrice && orderPrice > 0) {
+                        //拿到 id 打綠界
+                        const ecpayFormString = await checkoutEcpay(orderId);
+                        return ecpayFormString;
+                    } else {
+                        /** 免費課程，就不打綠界 */
+                        return ''
+                    }
 
+                } catch (error) {
+                    console.error('Failed to createOrder', error);
+                    throw error;
+                } finally {
+                    set((state) => {
+                        state.isLoading = false;
+                    });
+                }
+            },
+            resetStore: () => {
                 set((state) => {
-                    state.couponData = coupon;
-                    state.userData = user;
-                    state.courseData = course;
-                    state.orderData.id = id;
-                    state.orderData.originalPrice = originalPrice;
-                    state.orderData.orderPrice = orderPrice;
-                    state.orderData.status = status;
-                });
-            } catch (error) {
-                console.error('Failed to fetchOrder', error);
-                throw error;
-            } finally {
-                set((state) => {
+                    state.userData = initUserData;
+                    state.orderData = initOrderData;
+                    state.couponData = initCouponData;
+                    state.courseData = initCourseData;
                     state.isLoading = false;
+                    state.isShowDialog = false;
                 });
-            }
-        },
-        createOrder: async (requestData) => {
-            set((state) => {
-                state.isLoading = true;
-            });
-            try {
-                const newOrderResData = await createOrder(requestData);
-                const { id: orderId } = newOrderResData.data;
-
-                //拿到 id 打綠界
-                const ecpayFormString = await checkoutEcpay(orderId)
-                return ecpayFormString;
-            } catch (error) {
-                console.error('Failed to createOrder', error);
-                throw error;
-            } finally {
-                set((state) => {
-                    state.isLoading = false;
-                });
-            }
-        },
-        resetStore: () => {
-            set((state) => {
-                state.userData = initUserData;
-                state.orderData = initOrderData;
-                state.couponData = initCouponData;
-                state.courseData = initCourseData;
-                state.isLoading = false;
-                state.isShowDialog = false;
-            });
-        },
-    })
+            },
+        })),
+        {
+            name: "order-store",
+            storage: createJSONStorage(() => sessionStorage),
+            partialize: (state) => ({
+                userData: state.userData,
+                orderData: state.orderData,
+                couponData: state.couponData,
+                courseData: state.courseData,
+                isShowDialog: state.isShowDialog,
+            }),
+        }
     )
-)
+);
