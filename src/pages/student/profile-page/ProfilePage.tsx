8d45152cedffb1cf1f945ff7c1431f28f14bd7ac
ArvinYang <arvinyang@ArvinYangdeMacBook-Air.local>
2025-06-21 18:12:20 +0800

    fix: 🐛 remove console

--- src/pages/student/profile-page/ProfilePage.tsx
+++ src/pages/student/profile-page/ProfilePage.tsx
@@ -58,7 +58,6 @@ export default function ProfilePage() {
   };
 
   const handleUpdateFormData = async () => {
-    console.log('submit!!!!!')
     if (!validateProfile()) return;
 
     setIsLoading(true);
@@ -68,7 +67,6 @@ export default function ProfilePage() {
         phoneNumber: profile.phoneNumber,
       };
       const response = await updateProfile(data);
-      console.log("res", response)
       showCommonDialog({
         title: "儲存成功",
         description: response.status,
